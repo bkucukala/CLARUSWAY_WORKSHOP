eabf43d192247793e2c6f91073273b5923a5bb29
bkucukala <bkucukala@hotmail.com>
2022-02-07 16:49:24 +0100

    update

---
+++ Docker/Configmap/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import socket,os
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+greeting= os.environ['USERNAME']
+app = Flask(__name__)
+say=1
+user = "DevOps Team"
+version = "1.0"
+
+@app.route('/')
+def main_entry():
+    return render_template('index.html', message=greeting  ,ip = IPAddr, host_name = hostname, user = user, version = version )
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
